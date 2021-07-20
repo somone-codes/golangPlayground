a462a6b95007b9d2dc75fb78ede688fe9b2d233e
somone-codes <someone_mails@protonmail.com>
2021-07-20 15:11:20 +0530

    Section-1 using v3 version of rsc.io/quote alongside v1.

--- Section-1/moduleExample/hello.go
+++ Section-1/moduleExample/hello.go
@@ -2,8 +2,13 @@ package hello
 
 import (
 	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3" // using two versions of same lib
 )
 
 func Hello() string {
 	return quote.Hello()
+}
+
+func Proverb() string {
+	return quoteV3.Concurrency()
 }
