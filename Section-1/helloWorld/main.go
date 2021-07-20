c3065fcaf94f8435e528c1aa26c2570a3501e15b
somone-codes <someone_mails@protonmail.com>
2021-07-20 16:54:09 +0530

    Section-1 variables var

--- Section-1/helloWorld/main.go
+++ Section-1/helloWorld/main.go
@@ -2,12 +2,16 @@ package main
 
 var variable = 22
 
-func main() {
+func main() { // this bracket has to open here only else compilation fails
+	var variableWithTypeDef int // non usage of declared variable throws err
 	print("hello")
 
 	//short declaration operator
-	shortOp := "I am short op"
+	shortOp := "I am short op" // type is inferred and changing value of this var in future must be string only for string
 	println(shortOp)
 
-	println("We can use vars outside of function but no shorthand ops", variable)
+	println("We can use vars outside of function but no shorthand ops ", variable)
+
+	println("Variable with typedef have deefault values equal to 0 based on type store in them ",
+		variableWithTypeDef)
 }
