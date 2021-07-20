df4dd05f0c75fa4360a94cada55665acb42444d8
somone-codes <someone_mails@protonmail.com>
2021-07-20 16:46:29 +0530

    Section-1 variables var

--- Section-1/helloWorld/main.go
+++ Section-1/helloWorld/main.go
@@ -1,9 +1,13 @@
 package main
 
+var variable = 22
+
 func main() {
 	print("hello")
 
 	//short declaration operator
 	shortOp := "I am short op"
 	println(shortOp)
+
+	println("We can use vars outside of function but no shorthand ops", variable)
 }
