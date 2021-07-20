2bd39267a52c43e6c31e6f98f60dc0c06cb6b863
somone-codes <someone_mails@protonmail.com>
2021-07-20 17:12:06 +0530

    Section-1 get variables type and using fmt module for i/o

--- Section-1/helloWorld/main.go
+++ Section-1/helloWorld/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 var variable = 22
 
 func main() { // this bracket has to open here only else compilation fails
@@ -8,10 +10,12 @@ func main() { // this bracket has to open here only else compilation fails
 
 	//short declaration operator
 	shortOp := "I am short op" // type is inferred and changing value of this var in future must be string only for string
-	println(shortOp)
+	fmt.Println(shortOp)
 
-	println("We can use vars outside of function but no shorthand ops ", variable)
+	fmt.Println("We can use vars outside of function but no shorthand ops ", variable)
 
-	println("Variable with typedef have deefault values equal to 0 based on type store in them ",
+	fmt.Println("Variable with typedef have deefault values equal to 0 based on type store in them ",
 		variableWithTypeDef)
+
+	fmt.Printf("Type of variable shortOp is  %T ", shortOp)
 }
