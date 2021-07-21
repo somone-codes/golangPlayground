6ab1d74074771b8dc7f582ddd462e52866062175
somone-codes <someone_mails@protonmail.com>
2021-07-21 09:19:20 +0530

    Section-1 multi vars and more about unused vars and const

--- Section-1/helloWorld/main.go
+++ Section-1/helloWorld/main.go
@@ -2,15 +2,27 @@ package main
 
 import "fmt"
 
-var variable = 22
+var variable = 22 // vars here can go unused wont throw err only block ones throw err
 
 // x:=11 this is not allow, only allowed in block level
 
 const constant = "I am a const!"
 
+const (
+	const1         = 42
+	const2         = "I am a constant too!"
+	const3 float32 = 23.44
+)
+
+var (
+	multiVar1      = "I am a multi Var!"
+	multiVar2 int8 = 22
+)
+
 func main() { // this bracket has to open here only else compilation fails
 	var variableWithTypeDef int // non usage of declared variable throws err
 	print("hello")
+
 	//short declaration operator
 	shortOp := "I am short op" // type is inferred and changing value of this var in future must be string only for string
 	fmt.Println(shortOp)
@@ -35,4 +47,7 @@ func main() { // this bracket has to open here only else compilation fails
 	fmt.Print("Type conversion ", typeConversion)
 
 	fmt.Print(constant)
+
+	const constantDeclaredButUnused = 22 // we can have constants that are declared but unused but not vars
+
 }
