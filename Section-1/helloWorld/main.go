cf224ab80490828387564edb0349e891d2b27158
somone-codes <someone_mails@protonmail.com>
2021-07-21 09:44:56 +0530

    Section-1 iota

--- Section-1/helloWorld/main.go
+++ Section-1/helloWorld/main.go
@@ -19,6 +19,19 @@ var (
 	multiVar2 int8 = 22
 )
 
+const (
+	u         = iota      // u == 0     (untyped integer constant)  iota = 0
+	v float64 = iota * 42 // v == 42.0  (float64 constant)			 iota = 1
+	w         = iota      // w == 84    (untyped integer constant)	 iota = 2
+	a         = 1 << iota // a == 8							     iota = 3
+	b                     // b == 16		1 << a					 iota = 4
+	c                     // c == 32		1 << b					 iota = 5
+	d = iota              // d == 6								 iota = 6
+	e = 23                // e = 23								 iota = 7
+	f                     // f = 23								 iota = 8
+	g = iota              // g = 9									 iota = 9
+)
+
 func main() { // this bracket has to open here only else compilation fails
 	var variableWithTypeDef int // non usage of declared variable throws err
 	print("hello")
@@ -46,8 +59,25 @@ func main() { // this bracket has to open here only else compilation fails
 	var typeConversion int = int(customTypeVariable) // since underlying type of customType is int we can do this
 	fmt.Print("Type conversion ", typeConversion)
 
-	fmt.Print(constant)
+	fmt.Println(constant)
 
 	const constantDeclaredButUnused = 22 // we can have constants that are declared but unused but not vars
+
+	fmt.Println(u, v, w, a, b, c, d, e, f, g)
+
+	const (
+		b  = iota + 1
+		kb = 1 << (iota * 10) // left shift 1 by 10 times i.e  00000000 00000001 -> 00000100 00000000 i.e 1 becomes 1024
+		mb
+		gb
+	)
+
+	fmt.Printf("%d\t\t%b\n", b, b)
+	fmt.Printf("%d\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t%b\n", gb, gb)
+
+	const x = 10
+	fmt.Printf("%d\t%b\n", x, x)
 
 }
