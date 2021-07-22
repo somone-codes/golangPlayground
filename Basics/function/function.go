935ea1da6bbcf19346a46c1bc02ea985f556748e
somone-codes <someone_mails@protonmail.com>
2021-07-22 07:14:37 +0530

    Section-3 Functions with defer

--- Basics/function/function.go
+++ Basics/function/function.go
@@ -6,19 +6,23 @@ func main() {
 	combineParamsOfSameType("s1", "s2", 1)
 	withReturn("s1") // need not accept the return
 	s1, i1 := withMultipleReturns()
-	fmt.Print(s1, i1)
+	fmt.Println(s1, i1)
 	variadicParameter(1, 2, 3, 4)
+
+	defer withParams("I am called ", 2)
+	withParams("I am called ", 1) // with defer this line is executed before the func with deffer and on completion
+	// or failure defer func is called
 }
 
 //pass by value but if slice or array or etc is used then its reference/pointer is passed so changes will be seen by
 //caller
+//need not call all defined func
 func withParams(s1 string, i1 int) {
-	fmt.Print(s1, i1)
-	//need not call this func
+	fmt.Println(s1, i1)
 }
 
 func combineParamsOfSameType(s1, s2 string, i1 int) { // can combine vars of same type together optionally and put type
-	fmt.Print(s1, i1)
+	fmt.Println(s1, i1)
 	//need not use s2
 }
 
@@ -26,7 +30,7 @@ func combineParamsOfSameType(s1, s2 string, i1 int) { // can combine vars of sam
 //variadic param has to be the last param and can have other params before it
 func variadicParameter(i ...int) {
 
-	fmt.Printf("Type of i is %T, value is %v", i, i)
+	fmt.Printf("\nType of i is %T, value is %v\n", i, i)
 }
 
 func withReturn(s1 string) string {
