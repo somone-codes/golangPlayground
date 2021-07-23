0dfb0ef0defcb25fbd6a0bbfd347343743cf3bb5
somone-codes <someone_mails@protonmail.com>
2021-07-23 06:40:15 +0530

    Section-3 func returning func

--- Basics/function/function.go
+++ Basics/function/function.go
@@ -9,9 +9,15 @@ func main() {
 	fmt.Println(s1, i1)
 	variadicParameter(1, 2, 3, 4)
 
-	defer withParams("I am called ", 2)
-	withParams("I am called ", 1) // with defer this line is executed before the func with deffer and on completion
+	defer withParams("\nI am called ", 2) //executed last after all functions/lines below
+	withParams("I am called ", 1)         // with defer this line is executed before the func with deffer and on completion
 	// or failure defer func is called
+
+	// function that returns a function
+	x := functionReturnsFunc()
+	fmt.Printf("\n type of x is %T", x)
+	x(10)
+	functionReturnsFunc()(1)
 }
 
 //pass by value but if slice or array or etc is used then its reference/pointer is passed so changes will be seen by
@@ -39,4 +45,12 @@ func withReturn(s1 string) string {
 
 func withMultipleReturns() (string, int) {
 	return "I am a string", 1
+}
+
+//this function returns a func that take int, prints it and returns a bool
+func functionReturnsFunc() func(i int) bool {
+	return func(i int) bool {
+		fmt.Println("\n i is ", i)
+		return true
+	}
 }
